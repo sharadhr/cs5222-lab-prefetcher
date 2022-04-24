04756239f33e6ff3a440dfbbcf4aa2f42077e2bf
Sharadh Rajaraman <r.sharadh@yahoo.com.sg>
2022-04-24 19:30:35 +0800

    csv results

--- make_csv.sh
+++ make_csv.sh
@@ -7,5 +7,5 @@ for output_file in output/* ; do
     echo "${file_name}"
     arr=("${file_name//;/ }")
     ipc=$(jq --raw-input < "${output_file}" | jq --slurp '.[-3]' | jq 'split(" ")' | jq --raw-output '.[-1]')
-    echo "${arr[0]},${arr[1]},$ipc" >> results.csv
+    echo "${arr[0]},${arr[1]}$ipc" >> results.csv
 done
